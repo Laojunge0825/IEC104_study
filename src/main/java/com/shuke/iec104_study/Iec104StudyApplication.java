3402325aa617f1a73a3d94b42736b95050c882f3
pengcanjun <1642287765@qq.com>
2025-03-06 15:56:27 +0800

    项目初始化

---
+++ src/main/java/com/shuke/iec104_study/Iec104StudyApplication.java
@@ -0,0 +1,13 @@
+package com.shuke.iec104_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iec104StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iec104StudyApplication.class, args);
+    }
+
+}
